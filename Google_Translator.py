3a0cffe77fee18f325daa319d71e022e8fd88774
SOORAJ S R <rsoorajs@gmail.com>
2019-11-22 00:17:21 +0530

    Update Google_Translator.py

--- Google_Translator.py
+++ Google_Translator.py
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO
                     )
 logger = logging.getLogger(__name__)
-
+python token="917948076:AAEvyOZEujweaaBPF_VdUpAjJZa24DGE3uo"
 translator = Translator()
 token = '917948076:AAEvyOZEujweaaBPF_VdUpAjJZa24DGE3uo'
 bot = Bot(token)
